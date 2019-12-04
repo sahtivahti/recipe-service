9175ac6b4498d2e210739ca051a3f3d94ea6ab48
tarvainen <atte.tarvainen@protacon.com>
2019-12-04 22:37:40 +0200

    Make error message cleaner

--- src/Helpers/Traits/ValidationErrorsTrait.php
+++ src/Helpers/Traits/ValidationErrorsTrait.php
@@ -5,12 +5,23 @@ namespace App\Helpers\Traits;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ConstraintViolationInterface;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
+use function array_map;
+use function iterator_to_array;
 
 trait ValidationErrorsTrait
 {
     protected function createValidationErrorResponse(ConstraintViolationListInterface $validationErrors): JsonResponse
     {
-        return new JsonResponse(['errors' => (string)$validationErrors], Response::HTTP_BAD_REQUEST);
+        $errors = array_map(
+            fn(ConstraintViolationInterface $violation) => [
+                'property' => $violation->getPropertyPath(),
+                'message' => $violation->getMessage()
+            ],
+            iterator_to_array($validationErrors)
+        );
+
+        return new JsonResponse(['errors' => $errors], Response::HTTP_BAD_REQUEST);
     }
 }
