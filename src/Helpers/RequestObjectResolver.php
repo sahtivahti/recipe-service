49f11e4cb3f15b268bb649500e4d6147fca222f3
Atte Tarvainen <atte.tarvainen@protacon.com>
2019-12-04 21:27:56 +0200

    Use ArgumentValueResolver to resolve objects from the body (#31)
    * Add RequestObjectResolver
    
    * Rename deNormalizer to denormalizer
    
    * Actually throw exception

---
+++ src/Helpers/RequestObjectResolver.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Helpers;
+
+use Generator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class RequestObjectResolver implements ArgumentValueResolverInterface
+{
+    private DenormalizerInterface $denormalizer;
+
+    public function __construct(DenormalizerInterface $denormalizer)
+    {
+        $this->denormalizer = $denormalizer;
+    }
+
+    public function supports(Request $request, ArgumentMetadata $argument): bool
+    {
+        // Limit automatic resolving only for own classes
+        return strpos($argument->getType(), 'App\\') === 0 && class_exists($argument->getType());
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument): Generator
+    {
+        yield $this->denormalizer->denormalize($request->request->all(), $argument->getType());
+    }
+}
