e0760d5032f8f0d2ea08043226b90ba5fadd6a0e
tarvainen <atte.tarvainen@protacon.com>
2019-12-07 18:48:35 +0200

    Refactor documentation rendering and add tests

--- src/Controller/DocController.php
+++ src/Controller/DocController.php
@@ -21,41 +21,7 @@ class DocController extends AbstractController
      */
     public function actionSwaggerUi(): Response
     {
-        $content = <<<EOF
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="UTF-8">
-    <title>API Documentation</title>
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui.css">
-    <style>* { margin: 0; }</style>
-</head>
-<body>
-<div id="swagger-ui"></div>
-
-<script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-standalone-preset.js"></script>
-<script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-bundle.js"></script>
-
-<script>
-    window.onload = function() {
-        SwaggerUIBundle({
-            url: '/doc/swagger.json',
-            dom_id: '#swagger-ui',
-            deepLinking: true,
-            presets: [
-                SwaggerUIBundle.presets.apis,
-                SwaggerUIStandalonePreset
-            ],
-            plugins: [
-                SwaggerUIBundle.plugins.DownloadUrl
-            ],
-            layout: 'StandaloneLayout',
-        })
-    }
-</script>
-</body>
-</html>
-EOF;
+        $content = file_get_contents(__DIR__ . '/../../templates/docs.html');
 
         return new Response($content);
     }
