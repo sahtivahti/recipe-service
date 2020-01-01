39240e1ab74948214b1bfaa5406b496748b9ccf1
tarvainen <atte.tarvainen@protacon.com>
2020-01-01 11:20:47 +0200

    Add filtering to recipe listing route

---
+++ src/Model/RecipeSearchFilters.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Model;
+
+class RecipeSearchFilters
+{
+    private ?string $name = null;
+
+    private ?string $author = null;
+
+    private ?string $userId = null;
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     *
+     * @return self
+     */
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string|null $author
+     *
+     * @return self
+     */
+    public function setAuthor(?string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUserId(): ?string
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param string|null $userId
+     *
+     * @return self
+     */
+    public function setUserId(?string $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+}
