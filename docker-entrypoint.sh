816cb7c59fb5406f38bc76fdfba868ab1b00ba4d
tarvainen <atte.tarvainen@protacon.com>
2020-01-06 12:30:50 +0200

    Cache warmup part x

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 php /app/bin/console cache:warmup \
     && php /app/bin/console assets:install \
     && php /app/bin/console cache:clear \
-    && chmod -R 777 /app/var/cache
+    && chmod -R 777 /app/var
 
 # Run database migrations
 php /app/bin/console doctrine:migrations:migrate --no-interaction
