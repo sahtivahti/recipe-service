4497aaab29256505c3d4591cbf777bb089831227
tarvainen <atte.tarvainen@protacon.com>
2020-01-18 11:06:10 +0200

    Add tests for event constructors

---
+++ tests/Unit/Model/Event/RecipeCreatedEventTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Tests\Unit\Model\Event;
+
+use App\Entity\Recipe;
+use App\Model\Event\RecipeCreatedEvent;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class RecipeCreatedEventTest extends TestCase
+{
+    public function testThatConstructorSetsProperties(): void
+    {
+        $now = new DateTime();
+        $recipe = new Recipe();
+
+        $event = new RecipeCreatedEvent($recipe);
+
+        static::assertSame($recipe, $event->getItem());
+        static::assertSame($now->getTimestamp(), $recipe->getCreatedAt()->getTimestamp());
+        static::assertSame('recipe.created', $event->getType());
+    }
+}
---
+++ tests/Unit/Model/Event/RecipeUpdatedEventTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Tests\Unit\Model\Event;
+
+use App\Entity\Recipe;
+use App\Model\Event\RecipeUpdatedEvent;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class RecipeUpdatedEventTest extends TestCase
+{
+    public function testThatConstructorSetsProperties(): void
+    {
+        $now = new DateTime();
+        $recipe = new Recipe();
+
+        $event = new RecipeUpdatedEvent($recipe);
+
+        static::assertSame($recipe, $event->getItem());
+        static::assertSame($now->getTimestamp(), $recipe->getCreatedAt()->getTimestamp());
+        static::assertSame('recipe.updated', $event->getType());
+    }
+}
